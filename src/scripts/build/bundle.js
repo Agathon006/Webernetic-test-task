7d26a4101f609b4a1445f9d381a65ba3602722cc
Agathon006 <agaton615@gmail.com>
2024-06-11 02:51:00 +0300

    feat: add modal close/open logic by button

---
+++ src/scripts/build/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var t=document.querySelector("#try-button"),e=document.querySelector("#pop-up-message");t.addEventListener("click",(function(){e.classList.remove("not-visible")})),e.addEventListener("click",(function(t){t.target.hasAttribute("data-close")&&e.classList.add("not-visible")}))})();
+//# sourceMappingURL=bundle.js.map
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const tryButton = document.querySelector('#try-button');
+const popUpMessage = document.querySelector('#pop-up-message');
+
+tryButton.addEventListener('click', () => {
+    popUpMessage.classList.remove('not-visible');
+})
+
+popUpMessage.addEventListener('click', (e) => {
+    if (e.target.hasAttribute('data-close')) {
+        popUpMessage.classList.add('not-visible');
+    }
+})
