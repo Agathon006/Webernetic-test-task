18a779fa6b8c3ac7ba28b7074410f876687d6d27
Agathon006 <agaton615@gmail.com>
2024-06-11 04:28:26 +0300

    feat: add adaptive for all sizes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,10 +5,14 @@ const popUpMessage = document.querySelector('#pop-up-message');
 
 tryButton.addEventListener('click', () => {
     popUpMessage.classList.remove('not-visible');
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('mousedown', handleClickOutside);
 })
 
 popUpMessage.addEventListener('click', (e) => {
     if (e.target.hasAttribute('data-close')) {
         popUpMessage.classList.add('not-visible');
+        document.body.style.overflow = 'auto';
+        document.removeEventListener('mousedown', handleClickOutside);
     }
 })
